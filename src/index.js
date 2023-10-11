6bd3cf1279e0a39a9f9d1c290f26f0fb99c30199
Waliyullahi Lukman <mac@macs-MacBook-Air.local>
2023-10-12 00:02:10 +0100

    added

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@
 // const introduction = `Hi ${firstName}! Welcome to decagon, your stack is ${stack}`;
 // console.log(introduction);
 
-let firstName = "Waliyullahi";
-let lastName = "Lukman";
-let stack = "node";
-const introduction = `Hi ${firstName} ${lastName}! Welcome to decagon, your stack is ${stack}`;
-console.log(introduction);
+// let firstName = "Waliyullahi";
+// let lastName = "Lukman";
+// let stack = "node";
+// const introduction = `Hi ${firstName} ${lastName}! Welcome to decagon, your stack is ${stack}`;
+// console.log(introduction);
 
-let code = "Javascript code";
-let nickName = "Agba Coder";
-const introduction1 = `This is my first ${code} by ${nickName}`;
-console.log(introduction1);
+// let code = "Javascript code";
+// let nickName = "Agba Coder";
+// const introduction1 = `This is my first ${code} by ${nickName}`;
+// console.log(introduction1);
