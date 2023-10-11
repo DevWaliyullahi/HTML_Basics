3d1dce339209c6864b2f6e92f19f0144a5319f2a
Waliyullahi Lukman <mac@macs-MacBook-Air.local>
2023-10-11 11:43:54 +0100

    added

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// Hi Waliyullahi! Welcome to decagon, your stack is node
+Let firstName = "Waliyullahi";
+Let stack = "node"
+const introduction = `Hi ${firstname}! Welcome to decagon, your stack is ${stack}`
